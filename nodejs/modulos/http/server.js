66e51a80b0a3c29bca0bf76db76c7f36ac051e01
Maurício Scandolara <mauroscan20@gmail.com>
2025-10-03 10:57:10 -0300

    tratando rotas

---
+++ nodejs/modulos/http/server.js
@@ -0,0 +1,19 @@
+import http from 'http';
+
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Página inicial');
+  } else if (req.url === '/about' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Sobre nós');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Página não encontrada');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000');
+});
