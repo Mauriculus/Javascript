ad727c8cd4a84a4f9e9fb4ec1dcc24652cf226c6
Maurício Scandolara <mauroscan20@gmail.com>
2025-10-17 09:19:03 -0300

    rotas users e usuarios

--- nodejs/saudacao-mvc/controllers/siteController.js
+++ nodejs/saudacao-mvc/controllers/siteController.js
@@ -13,5 +13,8 @@ module.exports = {
     const { nome, idade, pet } = req.body;
     const mensagem = saudacaoModel.gerarMensagemPersonalizada(nome, idade, pet);
     res.send(`<h1>${mensagem}</h1>`);
+  },
+    usuarios: (req, res) => {
+    res.sendFile('usuarios.html', { root: './views' });
   }
 };
